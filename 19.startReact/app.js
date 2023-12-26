c083765195b897b599a39c1edce0a36411614648
shakilwebmaster <mdshakil102036@gmail.com>
2023-12-26 22:12:50 +0600

    javascript refresh

--- 19.startReact/app.js
+++ 19.startReact/app.js
@@ -1,10 +1,30 @@
+// var  lang = "Bangladesh";
+// const test = (topic) => {
+//     lang = topic;
+//     console.log(`I live in  ${topic}`);
 
+// }
+// test("India")
+// console.log(`I Love ${lang}`)
 
-var  lang = "Bangladesh";
-const test = (topic) => {
-    lang = topic;
-    console.log(`I live in  ${topic}`);
+    // ARRAY METHOD
+    const fruits = ["apple", "banana", "orange", "mango", "lemon"]
 
-}
-test("India")
-console.log(`I Love ${lang}`)
+    // fruits.find((f)=>{
+    //     console.log(f)
+    //     if(f === "orange" || f=== "mango"){
+    //        console.log(">>",f)
+    //     }
+    // })
+//     async function getvalue(){
+//         return "I Love Javascript"
+//     }
+// const result = getvalue();
+// console.log(result)
+
+// fruits.filter((fil)=>{
+    
+//     if(fil === "banana"){
+//         console.log(fil)
+//     }
+// })
