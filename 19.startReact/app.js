c3d6c8525b3e8c147be713663b46de4dcdec4fde
shakilwebmaster <mdshakil102036@gmail.com>
2023-12-27 23:52:21 +0600

    array method refresh

--- 19.startReact/app.js
+++ 19.startReact/app.js
@@ -8,7 +8,7 @@
 // console.log(`I Love ${lang}`)
 
     // ARRAY METHOD
-    const fruits = ["apple", "banana", "orange", "mango", "lemon"]
+    // const fruits = ["apple","apple", "banana", "orange", "mango", "lemon"]
 
     // fruits.find((f)=>{
     //     console.log(f)
@@ -28,3 +28,43 @@
 //         console.log(fil)
 //     }
 // })
+// const slice = fruits.slice(2,4)
+// console.log(slice)
+// console.log(fruits)
+// const splice = fruits.splice(2,4);
+// console.log(splice)
+// console.log(fruits)
+// const concat = fruits.concat(["Fruits 1", "Fruits 2"]);
+// console.log(concat)
+// console.log(fruits) ;
+// const push = fruits.push("shakil","Tanzu");
+// console.log("push"+ push);
+// console.log(fruits);
+// const map = fruits.map((item)=>{
+//     if(item === "apple"){
+//         return "Apple"
+//     }else{
+//         return "N/A"
+//     }
+// })
+
+// console.log(map);
+// const numbers = [1,2,3,4,5,6,7,8,9];
+// const addition = numbers.reduce((total, currValue)=>{
+//     total + currValue
+// },0)
+// console.log(addition)
+const language = {
+    name: "Shakil Hossain",
+    age: 23,
+    job: "Frontend designer",
+    company:"coder71"
+}
+console.log(Object.keys(language))
+console.log(Object.values(language))
+console.log(Object.entries(language))
+
+const study = "Student"
+language[study] = "I am Student";
+console.log(language)
+  
